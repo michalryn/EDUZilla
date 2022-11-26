b7f1f8a04b598a0d2f26c7d281e57547483dbe0e
Michał Ryniewicz <misiekxyz18@gmail.com>
2022-11-26 19:04:28 +0100

    fixed language change when not logged in

--- Controllers/LanguageController.cs
+++ Controllers/LanguageController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Localization;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EDUZilla.Controllers
 {
+    [AllowAnonymous]
     public class LanguageController : Controller
     {
         public IActionResult Change(string culture)
