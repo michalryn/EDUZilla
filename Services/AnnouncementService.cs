59db9d384bf1f08c5896e8cb6987ceb3bcbeb989
User <User@DESKTOP-HAF5E63>
2023-01-26 15:24:00 +0100

    zmiany

--- Services/AnnouncementService.cs
+++ Services/AnnouncementService.cs
@@ -7,23 +7,25 @@ namespace EDUZilla.Services
     public class AnnouncementService
     {
         private readonly AnnouncementRepository _announcementRepository;
-
-        public AnnouncementService(AnnouncementRepository announcementRepository)
+        private readonly ClassRepository _classRepository;
+        public AnnouncementService(AnnouncementRepository announcementRepository, ClassRepository classRepository)
         {
             _announcementRepository = announcementRepository;
+            _classRepository = classRepository;
         }
 
         public async Task<bool> AddAnnouncementAsync(AnnouncementViewModel announcementViewModel)
         {
             try
             {
+                var group = _classRepository.GetClassById(announcementViewModel);
                 Announcement announcement = new Announcement()
                 {
                     Topic = announcementViewModel.Topic,
                     Content = announcementViewModel.Content,
                     CreatedDate = announcementViewModel.Created,
                     Sender = announcementViewModel.Sender,
-                    Receiver = announcementViewModel.Class
+                    Receiver = group
                 };
                 await _announcementRepository.AddAndSaveChangesAsync(announcement);
             }
